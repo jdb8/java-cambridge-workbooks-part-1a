ce10b4203212dc5c1ce1c8aeb58824e9fca74e83
Joe Bateson <jdb75@cam.ac.uk>
2012-01-20 16:13:00 +0000

    Changed PrimitiveInt to work out the answer to question 1 - added the answer to answers.txt

--- uk/ac/cam/jdb75/examples/PrimitiveInt.java
+++ uk/ac/cam/jdb75/examples/PrimitiveInt.java
@@ -2,12 +2,9 @@ package uk.ac.cam.jdb75.examples;
 
 class PrimitiveInt {
 	public static void main(String[] args) {
-		int z = 1<<5;
-		int x = 1<<5;
-		int y = 1<<6;
-		int right = (z & x) >> 5;
-		int wrong = (z & y) >> 5;
-		System.out.println(right);
-		System.out.println(wrong);
+		int a = 4000;
+		int b = 2;
+		int c = 1;
+		System.out.println(c + (a=b));
 	}
 }
