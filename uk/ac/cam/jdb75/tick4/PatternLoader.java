38b23fce7ecf24b8f2d0a5671c407e86f48c860c
Joe Bateson <jdb75@cam.ac.uk>
2012-02-09 18:11:34 +0000

    Started PatternLoader.java

---
+++ uk/ac/cam/jdb75/tick4/PatternLoader.java
@@ -0,0 +1,21 @@
+package uk.ac.cam.jdb75.tick4;
+
+import java.io.Reader;
+import java.io.IOException;
+import java.util.List;
+
+public class PatternLoader {
+
+    public static List<Pattern> load(Reader r) throws IOException {
+        //TODO: Complete the implementation of this method.
+        List<Pattern> resultList = new LinkedList<Pattern>();
+
+        for(Pattern p: resultList) {
+            //p references each element of "resultList" in order so that first time
+            //round the loop, p references the first element, second time round the 
+            //second element, and so on. The loop terminates when "resultList" has
+            //no more elements.
+        }
+
+    }
+}   
