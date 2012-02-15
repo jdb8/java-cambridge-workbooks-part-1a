e5a2b314aab448656fbf8163fe4422034088c0df
Joe Bateson <jdb75@cam.ac.uk>
2012-02-15 22:28:40 +0000

    Implemented basic PatternLoader.java (q7), not tested (but compiles)

--- uk/ac/cam/jdb75/tick4/PatternLoader.java
+++ uk/ac/cam/jdb75/tick4/PatternLoader.java
@@ -1,21 +1,28 @@
 package uk.ac.cam.jdb75.tick4;
 
 import java.io.Reader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
+import java.util.LinkedList;
 
 public class PatternLoader {
 
     public static List<Pattern> load(Reader r) throws IOException {
         //TODO: Complete the implementation of this method.
+        String line; 
         List<Pattern> resultList = new LinkedList<Pattern>();
+        BufferedReader buff = new BufferedReader(r);
 
-        for(Pattern p: resultList) {
-            //p references each element of "resultList" in order so that first time
-            //round the loop, p references the first element, second time round the 
-            //second element, and so on. The loop terminates when "resultList" has
-            //no more elements.
+        while ((line = buff.readLine()) != null) {            
+            try {
+                Pattern p = new Pattern(line);
+                resultList.add(p);
+            } catch (PatternFormatException e) {
+                // do nothing
+            }         
         }
 
+        return resultList;        
     }
 }   
