4b8abcfbd6aa66f518ac1f9b24b05a5626594fbc
Joe Bateson <jdb75@cam.ac.uk>
2012-02-16 16:28:47 +0000

    Better method for PatternFormatException, using super(message)

--- uk/ac/cam/jdb75/tick4/PatternFormatException.java
+++ uk/ac/cam/jdb75/tick4/PatternFormatException.java
@@ -2,13 +2,7 @@ package uk.ac.cam.jdb75.tick4;
 
 public class PatternFormatException extends Exception {
 
-	private String message;
-	
 	public PatternFormatException(String message) {
-		this.message = message;
-	}
-
-	public String getMessage() {
-		return this.message;
+		super(message);
 	}
 }
