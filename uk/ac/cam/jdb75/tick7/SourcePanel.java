0379bbde14340acd13b3c3136e7f16e42aeb8673
Joe Bateson <jdb75@cam.ac.uk>
2012-03-01 18:06:40 +0000

    Steps 25-28: made SourcePanel abstract, GUILife does not compile (steps 26+ should deal with this)

--- uk/ac/cam/jdb75/tick7/SourcePanel.java
+++ uk/ac/cam/jdb75/tick7/SourcePanel.java
@@ -1,19 +1,52 @@
 package uk.ac.cam.jdb75.tick7;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
 
-public class SourcePanel extends JPanel {
+public abstract class SourcePanel extends JPanel {
+    
+    private JRadioButton current;
 
     public SourcePanel() {
         super();
         setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
-        JRadioButton none = new JRadioButton(Strings.BUTTON_SOURCE_NONE, true);
-        JRadioButton file = new JRadioButton(Strings.BUTTON_SOURCE_FILE, true);
-        JRadioButton library = new JRadioButton(Strings.BUTTON_SOURCE_LIBRARY, true);
+        final JRadioButton none = new JRadioButton(Strings.BUTTON_SOURCE_NONE, true);
+        none.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (setSourceNone())
+                    current = none; 
+                else
+                    //unsuccessful: re-enable previous source choice
+                    current.setSelected(true); 
+                }
+        });
+        final JRadioButton file = new JRadioButton(Strings.BUTTON_SOURCE_FILE, true);
+        file.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (setSourceFile())
+                    //successful: file found and patterns loaded
+                    current = file; 
+                else
+                    //unsuccessful: re-enable previous source choice
+                    current.setSelected(true); 
+                }
+        });
+        final JRadioButton library = new JRadioButton(Strings.BUTTON_SOURCE_LIBRARY, true);
+        library.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (setSourceLibrary())
+                    current = library; 
+                else
+                    //unsuccessful: re-enable previous source choice
+                    current.setSelected(true); 
+                }
+        });
         JRadioButton fourStar = new JRadioButton(Strings.BUTTON_SOURCE_FOURSTAR, true);  
         //add RadioButtons to this JPanel
         add(none);
@@ -27,5 +60,10 @@ public class SourcePanel extends JPanel {
         group.add(file);
         group.add(library);
         group.add(fourStar);
+        current = none;
     }
+    
+    protected abstract boolean setSourceNone();
+    protected abstract boolean setSourceFile();
+    protected abstract boolean setSourceLibrary();
 }
