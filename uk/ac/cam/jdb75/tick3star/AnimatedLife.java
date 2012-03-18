325498746fa689f7ba5c23ecd33719018ada4bbc
Joe Bateson <jdb75@cam.ac.uk>
2012-03-18 17:55:23 +0000

    Carried on working with tick3star - gif with colour, history etc

---
+++ uk/ac/cam/jdb75/tick3star/AnimatedLife.java
@@ -0,0 +1,107 @@
+package uk.ac.cam.jdb75.tick3star;
+
+import java.io.IOException;
+
+public class AnimatedLife {
+    
+    private static OutputAnimatedGif gif = null;
+    
+    private static String formatString = null;
+    static int generations = 0;
+    static String filename = null;
+    
+    public static void main(String[] args) {        
+        try {
+            if (args.length != 3) {
+                System.out.println("Error: 3 arguments are required: String world, int generations, String filename"); 
+                return;
+            }
+            formatString = args[0];
+            generations = Integer.parseInt(args[1]);
+            filename = args[2];
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Second argument is not an integer.");
+            return;
+        }
+        
+        Pattern p = new Pattern(formatString); 
+        boolean[][] world = new boolean[p.getHeight()][p.getWidth()];
+        p.initialise(world);
+        
+        try {
+            gif = new OutputAnimatedGif(filename);
+            play(world);
+            gif.close();
+        } catch (IOException e) {
+            System.out.println("Error: There was a problem writing to the disk.");
+            return;
+        }        
+    }
+    
+    public static boolean getCell(boolean[][] world, int col, int row) {
+        return (row < 0 || row > world.length - 1 || col < 0 || col > world[row].length - 1) ? false : world[row][col];
+    }
+
+    public static void setCell(boolean[][] world, int col, int row, boolean value) {
+        if (row < 0 || row > world.length - 1 || col < 0 || col > world[row].length - 1) {} 
+        else { world[row][col] = value; }
+    }
+
+    public static int countNeighbours(boolean[][] world, int col, int row) {
+        int neighbours = 0;
+        
+        // increase neighbours by 1 if getCell is true for each adjacent square 
+        neighbours += (getCell(world, col-1, row-1)) ? 1 : 0;
+        neighbours += (getCell(world, col, row-1)) ? 1 : 0;
+        neighbours += (getCell(world, col+1, row-1)) ? 1 : 0;
+        neighbours += (getCell(world, col-1, row)) ? 1 : 0;
+        neighbours += (getCell(world, col+1, row)) ? 1 : 0;
+        neighbours += (getCell(world, col-1, row+1)) ? 1 : 0;
+        neighbours += (getCell(world, col, row+1)) ? 1 : 0;
+        neighbours += (getCell(world, col+1, row+1)) ? 1 : 0;
+
+        return neighbours;
+    }
+
+    public static boolean computeCell(boolean[][] world, int col, int row) {
+
+        boolean liveCell = getCell(world, col, row);
+        int neighbours = countNeighbours(world, col, row);
+        boolean nextCell = false;
+        
+        if (neighbours < 2) {
+            nextCell = false;
+        }
+        if (liveCell && neighbours == 2 || liveCell && neighbours == 3) {
+            nextCell = true;
+        }
+        if (neighbours > 3) {
+            nextCell = false;
+        }
+        if (!liveCell && neighbours == 3) {
+            nextCell = true;
+        }
+    
+        return nextCell;
+    }
+
+    public static boolean[][] nextGeneration(boolean[][] world) {
+        boolean[][] newWorld = new boolean[world.length][];
+
+        for (int row = 0; row < world.length; row++) {
+            newWorld[row] = new boolean[world[row].length];
+            for (int col = 0; col < world[row].length; col++) {
+                setCell(newWorld, col, row, computeCell(world, col, row));
+            }
+        }
+        return newWorld;
+    }
+
+    public static void play(boolean[][] world) throws IOException {
+        for (int i = 0; i<generations; i++) {
+            gif.addFrame(world);
+            world = nextGeneration(world);
+        }
+    }
+
+}
--- uk/ac/cam/jdb75/tick3star/OutputAnimatedGif.java
+++ uk/ac/cam/jdb75/tick3star/OutputAnimatedGif.java
@@ -12,6 +12,8 @@ public class OutputAnimatedGif {
 
     private FileImageOutputStream output;
     private ImageWriter writer;
+    private float hue = 0; // current hue of colour
+    private int[][] prevIntWorld;
 
     public OutputAnimatedGif(String file) throws IOException {
         this.output = new FileImageOutputStream(new File(file)); 
@@ -23,16 +25,49 @@ public class OutputAnimatedGif {
     private BufferedImage makeFrame(boolean[][] world) {
         int w = world[0].length; // assuming a rectangular or square board - no weird shapes!
         int h = world.length;
-        int cellSize = 10; // size of each cell square
+        int cellSize = 20; // size of each cell square
+        float s = 0.8f; // colour saturation 
+        float b = 0.3f; // colour brightness
+        int cellMemory = 10;
+        
+        if (prevIntWorld == null) {
+            prevIntWorld = new int[h][w];
+        }
+        
+        int[][] prevIntWorldCopy = prevIntWorld; // copy prevIntWorld so we can update it while accessing old elements
+        
+        Color c = Color.getHSBColor(hue, s, b);
+        hue += 0.01f;
 
         BufferedImage image = new BufferedImage(w*cellSize, h*cellSize, BufferedImage.TYPE_INT_RGB);
-
         Graphics g = image.getGraphics(); //create a new graphics context
-        g.setColor(Color.black);
-        g.drawRect(20, 20, 10, 10);
+        
+        g.setColor(c);
+        g.fillRect(0, 0, w*cellSize, h*cellSize); // set the background with the current HSB value
+        
+        for (int row = 0; row < world.length; row++) {
+            for (int col = 0; col < world[row].length; col++) {
+                float cur = (float)prevIntWorldCopy[row][col];
+                if (cur != 0) {
+                    cur -= 1;
+                    prevIntWorld[row][col] -= 1;
+                    g.setColor(new Color(255, 255, 255, (int)(70/10 *cur)));
+                    g.fillRect(col*cellSize, row*cellSize, cellSize, cellSize);
+                }
+                if (AnimatedLife.getCell(world, col, row)) {
+                    g.setColor(Color.black);
+                    g.fillRect(col*cellSize+2, row*cellSize+2, cellSize, cellSize); // shadow
+                    g.setColor(Color.white);
+                    g.fillRect(col*cellSize, row*cellSize, cellSize, cellSize);
+                    g.setColor(Color.black);
+                    g.drawRect(col*cellSize, row*cellSize, cellSize, cellSize); //border
+                    prevIntWorld[row][col] = cellMemory;
+                }                
+            }
+        }
 
         g.dispose(); // free up resources used by the graphics context
-
+        
         return image;
     }
     
@@ -70,21 +105,5 @@ public class OutputAnimatedGif {
 
     public void close() throws IOException {
         writer.endWriteSequence();
-    }
-
-    public static void main(String[] args) throws IOException {
-        
-    }
-
-    public void test() throws IOException {
-        new OutputAnimatedGif("test.gif");
-
-        //temporary testing code
-        //Pattern p = new Pattern(args[0]);
-        boolean[][] world = new boolean[80][80];
-        makeFrame(world);
-        addFrame(world);
-        close();
-        //p.initialise(world);
     }
 }
